a8f279b8a64e5c1a55cde5fd241cfea5cfaa91b5
st843391622 <843391622@qq.com>
2019-07-15 11:02:57 +0800

    update 7.15

--- electron-vue-music/src/views/main.vue
+++ electron-vue-music/src/views/main.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-        内容
+        <iframe src="https://www.kugou.com/yy/html/rank.html" frameborder="0" width="100%" height="100%"></iframe>
     </div>
 </template>
 
 <script>
 export default {
-    
+    methods: {
+        setIframeHeight(iframe) {
+            iframe.οnlοad=function(){
+                var bHeight = iframe.contentWindow.document.body.scrollHeight;
+                var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+                var height = Math.min(bHeight, dHeight);
+                iframe.height = height;
+	        }
+        }
+    },
+    created() {
+        this.setIframeHeight(document.getElementById('iframeContent'))
+    }
 }
 </script>
 
